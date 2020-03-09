1380b89cc3086e4d88cbdeff711f781d53a45860
lauratamath <laurylevi@hotmail.com>
2020-03-08 20:29:11 -0600

    Ignorar el otro txt, solo es para tomar referenicia

---
+++ InterfaceMap.java
@@ -0,0 +1,55 @@
+/**
+ * @author Orlando Cabrera
+ * @author Laura Tamath
+ *@since 08/03/2020
+ *@version /03/2020
+ * Interfaz que contiene los metodos a utilizar
+ */
+import java.util.*;
+public interface InterfaceMap<K, V>{
+	/** En donde:
+	* @param k  Llave
+	* @param v  Valor
+	**/
+
+	/**
+	Pre: Con la ayuda de un map se obtiene el tamaño
+	Post: Se obtiene el tamaño del map
+	*/
+	public int size();
+
+    /**
+	Pre: Se cuenta con una llave y un valor
+	Post: Se asigna un valor a la llave
+	*/
+	public V put(K k, V v);
+
+	/**
+	Pre: La llave existe
+	Post: Retorna el valor asignado a la llave
+	*/	
+    public V get(K k);
+
+
+	/**
+	Pre: La llave existe
+	Post: Se remueve el valor de la llave y  la llave
+	*/
+	public V remove(K k);
+
+	
+	/**
+	Pre: La llave existe
+	Post: Retorna verdadero si la llave se encuentra en el mapa
+	*/	
+	public boolean containsKey(K k);
+
+	/**
+	Pre: El valor existe
+	Post: Retorna verdadero si el valor se encuentra en el mapa
+	*/	
+	public boolean containsValue(V v);
+
+
+	public Set<Map.Entry<K,V>> entrySet();
+}

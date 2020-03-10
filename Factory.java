9fa9a1df6c9c18fd377f2b3fa5b76d49512db44f
Orlando Cabrera <orlando.osberto@gmail.com>
2020-03-09 21:24:12 -0600

    Factory hecho

---
+++ Factory.java
@@ -0,0 +1,21 @@
+/**
+ * @author Orlando Cabrera
+ * @author Laura Tamath
+ * @since 08/03/2020
+ * @version /03/2020
+ * Factory
+ */
+public class Factory<K,V> {
+	public InterfaceMap<K,V> getMap (String tipo){
+		switch (tipo){
+			case "1":
+				return new Hash<K,V>();
+			case "2":
+				return new Linked<K,V>();
+			case "3":
+				return new Tree<K,V>();
+		}
+		return null;
+		
+	}
+}
